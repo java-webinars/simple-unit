500efea03aea7d17e2c7d6255dbe0bb199dee4aa
java-webinars <chestrick@yandex.ru>
2022-05-30 17:31:54 -0400

    Lesson_0002

---
+++ src/main/java/edu/javacourse/unit/manager/DocumentManager.java
@@ -0,0 +1,8 @@
+package edu.javacourse.unit.manager;
+
+public class DocumentManager
+{
+    public void uploadFile() {
+        System.out.println("Called");
+    }
+}
---
+++ src/test/java/edu/javacourse/unit/manager/DocumentManagerTest.java
@@ -0,0 +1,13 @@
+package edu.javacourse.unit.manager;
+
+
+import org.junit.jupiter.api.Test;
+
+class DocumentManagerTest
+{
+    @Test
+    void uploadFile() {
+        DocumentManager dm = new DocumentManager();
+        dm.uploadFile();
+    }
+}
