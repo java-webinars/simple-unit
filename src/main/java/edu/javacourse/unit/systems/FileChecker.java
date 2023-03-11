ab67f0b226e3ddf8611614d328ebd70edf944e6c
java-webinars <chestrick@yandex.ru>
2023-03-11 13:57:49 -0500

    Lesson_0004_2

--- src/main/java/edu/javacourse/unit/systems/FileChecker.java
+++ src/main/java/edu/javacourse/unit/systems/FileChecker.java
@@ -1,7 +1,10 @@
 package edu.javacourse.unit.systems;
 
+import org.springframework.stereotype.Service;
+
 import java.io.InputStream;
 
+@Service
 public class FileChecker
 {
     public boolean checkFile(InputStream file, String fileName) {
--- src/test/java/edu/javacourse/unit/manager/DocumentManagerTest.java
+++ src/test/java/edu/javacourse/unit/manager/DocumentManagerTest.java
@@ -1,12 +1,37 @@
 package edu.javacourse.unit.manager;
 
+import edu.javacourse.unit.systems.AlfrescoSystem;
+import edu.javacourse.unit.systems.FileChecker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.io.ByteArrayInputStream;
 
 class DocumentManagerTest
 {
+    public static final Long COMPANY_ID = 1L;
+
+    private DocumentManager documentManager;
+
+    @BeforeEach
+    void setUp() {
+        initDocumentManager();
+    }
+
     @Test
     void uploadFile() {
-        DocumentManager dm = new DocumentManager();
-        dm.uploadFile(null, null, null, true, true);
+        ByteArrayInputStream ba = new ByteArrayInputStream(new byte[20]);
+        documentManager.uploadFile(ba, "test.txt", COMPANY_ID, true, true);
+    }
+
+    private void initDocumentManager() {
+        documentManager = new DocumentManager();
+
+        FileChecker fileChecker = new FileChecker();
+        ReflectionTestUtils.setField(documentManager, "fileChecker", fileChecker);
+
+        AlfrescoSystem alfrescoSystem = new AlfrescoSystem();
+        ReflectionTestUtils.setField(documentManager, "alfrescoSystem", alfrescoSystem);
     }
 }
